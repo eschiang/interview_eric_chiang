aa86b4e47f3ec4b7578921b8fdbd106a365631f5
Eric Chiang <ericchiang@yahoo-inc.com>
2016-08-31 16:52:41 -0700

    cleanup v0.1.0

--- app/src/main/java/com/example/interview/interview/MainActivityFragment.java
+++ app/src/main/java/com/example/interview/interview/MainActivityFragment.java
@@ -1,7 +1,6 @@
 package com.example.interview.interview;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,14 +12,7 @@ import android.view.ViewGroup;
 
 import com.example.interview.interview.adapters.PhotoAdapter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -36,8 +28,6 @@ import okhttp3.Response;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-    private static final int CONNECTION_TIMEOUT = 10000;
-    private static final int READ_TIMEOUT = 15000;
     private static final String TAG = "MAIN_ACTIVITY_FRAGMENT";
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -50,7 +40,27 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        
+        makeNetworkCall();
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAdapter = new PhotoAdapter();
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void makeNetworkCall(){
         OkHttpClient client = new OkHttpClient();
         HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.500px.com/v1/photos?feature=fresh_today&sort=created_at&page=1&limit=10&image_size=4&include_store=store_download&include_states=voted&consumer_key=mSDECDmxoEEEw32OgaNxZxhUFuwiZetUaK9xTyTW").newBuilder();
         String url = urlBuilder.build().toString();
@@ -74,120 +84,6 @@ public class MainActivityFragment extends Fragment {
             });
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        return view;
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mAdapter = new PhotoAdapter();
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
-        mLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
-    }
-
-    private class AsyncFetch extends AsyncTask<String, String, String> {
-        HttpURLConnection conn;
-        URL url = null;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                url = new URL("https://api.500px.com/v1/photos?feature=fresh_today&sort=created_at&page=1&limit=10&image_size=4&include_store=store_download&include_states=voted&consumer_key=mSDECDmxoEEEw32OgaNxZxhUFuwiZetUaK9xTyTW");
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(READ_TIMEOUT);
-                conn.setConnectTimeout(CONNECTION_TIMEOUT);
-                conn.setRequestMethod("GET");
-                conn.setDoOutput(true);
-                conn.connect();
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-                return e1.toString();
-            }
-
-            try {
-
-                int response_code = conn.getResponseCode();
-
-                // Check if successful connection made
-                if (response_code == HttpURLConnection.HTTP_OK) {
-
-                    // Read data sent from server
-                    InputStream input = conn.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder result = new StringBuilder();
-                    String line;
-
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-
-                    // Pass data to onPostExecute method
-                    return (result.toString());
-
-                } else {
-
-                    return ("unsuccessful");
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return e.toString();
-            } finally {
-                conn.disconnect();
-            }
-
-
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d(TAG, result);
-            //this method will be running on UI thread
-            /*
-            List<DataFish> data = new ArrayList<>();
-
-            pdLoading.dismiss();
-            try {
-
-                JSONArray jArray = new JSONArray(result);
-
-                // Extract data from json and store into ArrayList as class objects
-                for (int i = 0; i < jArray.length(); i++) {
-                    JSONObject json_data = jArray.getJSONObject(i);
-                    DataFish fishData = new DataFish();
-                    fishData.fishImage = json_data.getString("fish_img");
-                    fishData.fishName = json_data.getString("fish_name");
-                    fishData.catName = json_data.getString("cat_name");
-                    fishData.sizeName = json_data.getString("size_name");
-                    fishData.price = json_data.getInt("price");
-                    data.add(fishData);
-                }
-
-                // Setup and Handover data to recyclerview
-                mRVFishPrice = (RecyclerView) findViewById(R.id.fishPriceList);
-                mAdapter = new AdapterFish(MainActivity.this, data);
-                mRVFishPrice.setAdapter(mAdapter);
-                mRVFishPrice.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-            } catch (JSONException e) {
-                Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-            }*/
-
         }
     }
 }
